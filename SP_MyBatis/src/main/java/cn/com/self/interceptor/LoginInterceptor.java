4803ac33edfe65a814ad40ddb5ad368453ea2f4c
Prime <30680967+9010@users.noreply.github.com>
2019-01-18 15:44:47 +0800

    Change

---
+++ SP_MyBatis/src/main/java/cn/com/self/interceptor/LoginInterceptor.java
@@ -0,0 +1,24 @@
+package cn.com.self.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler){
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           ModelAndView modelAndView) throws Exception{}
+
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler, Exception ex) throws Exception{}
+}
